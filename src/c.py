24faa3b3092bdcd6be92d680c3fe905f8d85d90c
mo3taz1705 <moutaz.samir@outlook.com>
2017-05-20 18:13:50 +0200

    loops statements added but without quadrubles

--- src/c.py
+++ src/c.py
@@ -5,7 +5,10 @@
 ###############################################################################
 
 register_id = 0
+label_id = 1
+line_no = 1
 out_file = None
+newLine_pos = None
 
 # Token types
 #
@@ -49,6 +52,19 @@ DIVIDE        = 'DIVIDE'
 ID            = 'ID'
 EOF           = 'EOF'
 
+def locations_of_substring(string, substring):
+    """Return a list of locations of a substring."""
+
+    substring_length = len(substring)    
+    def recurse(locations_found, start):
+        location = string.find(substring, start)
+        if location != -1:
+            return recurse(locations_found + [location], location+substring_length)
+        else:
+            return locations_found
+
+    return recurse([], 0)
+
 class Token(object):
     def __init__(self, type, value):
         self.type = type
@@ -89,10 +105,14 @@ class Lexer(object):
         self.current_char = self.text[self.pos]
 
     def error(self):
-        raise Exception('Lexical Error: Invalid character')
+        raise Exception('Lexical Error: Invalid character at line: ' + str(line_no))
 
     def advance(self):
         """Advance the `pos` pointer and set the `current_char` variable."""
+        global line_no
+        if len(newLine_pos) > 0 and self.pos > newLine_pos[0]:
+            line_no = line_no + 1
+            del newLine_pos[0]
         self.pos += 1
         if self.pos > len(self.text) - 1:
             self.current_char = None  # Indicates end of input
@@ -233,6 +253,7 @@ class Lexer(object):
                 self.advance()
                 return Token(DIVIDE, '/')
 
+
             self.error()
 
         return Token(EOF, None)
@@ -255,6 +276,11 @@ class BinOp(AST):
         self.right = right
 
 class Number(AST):
+    def __init__(self, token):
+        self.token = token
+        self.value = token.value
+
+class Boolean(AST):
     def __init__(self, token):
         self.token = token
         self.value = token.value
@@ -286,12 +312,52 @@ class Program(AST):
 class NoOp(AST):
     pass
 
+class SwitchStat(AST):
+    def __init__(self, var, case_stats, cmpd_stats):
+        self.var = var
+        self.case_stats = case_stats
+        self.cmpd_stats = cmpd_stats
+
 class VarDecl(AST):
     def __init__(self, var, type, exp, isConst):
         self.left = var
         self.var_type = type
         self.right = exp
         self.isConst = isConst
+
+class IfStat(AST):
+    def __init__(self, expr, cmpd1, cmpd2):
+        self.expr = expr
+        self.cmpd1 = cmpd1
+        self.cmpd2 = cmpd2
+
+class WhileStat(AST):
+    def __init__(self, expr, cmpd):
+        self.expr = expr
+        self.cmpd1 = cmpd
+
+class DoWhileStat(AST):
+    def __init__(self, expr, cmpd):
+        self.expr = expr
+        self.cmpd1 = cmpd
+
+class BoolOp(AST):
+    def __init__(self, left, op, right):
+        self.left = left
+        self.token = self.op = op
+        self.right = right
+
+class ForStat(AST):
+    def __init__(self, init, middle, end, cmpd_stat):
+        self.init = init
+        self.middle = middle
+        self.end = end
+        self.cmpd_stat = cmpd_stat
+
+class BreakStat(AST):
+    def __init__(self, token):
+        self.token = token
+        self.value = token.value
 
 class Type(AST):
     def __init__(self, token):
@@ -305,7 +371,7 @@ class Parser(object):
         self.current_token = self.lexer.get_next_token()
 
     def error(self):
-        raise Exception('Syntax Error: Invalid syntax')
+        raise Exception('Syntax Error: Invalid syntax at line: ' + str(line_no))
 
     def eat(self, token_type):
         # compare the current token type with the passed token
@@ -354,23 +420,28 @@ class Parser(object):
         node = self.statement()
         results = [node]
 
-        while self.current_token.type in (L_BRACE, ID, INTEGER_TYPE, CONSTANT, SEMI_COLON):
+        while self.current_token.type in (BREAK, IF, SWITCH, WHILE, DO, FOR, ID, INTEGER_TYPE, CONSTANT, SEMI_COLON):
             results.append(self.statement())
 
         return results
 
     def statement(self):
         """
-        statement : compound_statement SEMI_COLON
+        statement : BREAK SEMI_COLON
+                  | selection_statement
+                  | iteration_statement
                   | assignment_statement SEMI_COLON
                   | declaration_statement SEMI_COLON
                   | const_statement SEMI_COLON
                   | SEMI_COLON
         """
         node = NoOp()
-        if self.current_token.type == L_BRACE:
-            node = self.compound_statement()
-            self.eat(SEMI_COLON)
+        if self.current_token.type == BREAK:
+            node = BreakStat()
+        elif self.current_token.type in (IF, SWITCH):
+            node = self.selection_statement()
+        elif self.current_token.type in (WHILE, DO, FOR):
+            node = self.iteration_statement()
         elif self.current_token.type == ID:
             node = self.assignment_statement()
             self.eat(SEMI_COLON)
@@ -385,12 +456,27 @@ class Parser(object):
 
         return node
 
+    def init_for(self):
+        """
+        init_for : assignment_statement
+                 | declaration_statement
+        """
+        node = NoOp()
+        if self.current_token.type == ID:
+            node = self.assignment_statement()
+        elif self.current_token.type == INTEGER_TYPE:
+            node = self.variable_declaration()
+        return node
+
     def compound_statement(self):
         """
         compound_statement: L_BRACE statement_list R_BRACE
+                          | L_BRACE R_BRACE
         """
+        nodes = []
         self.eat(L_BRACE)
-        nodes = self.statement_list()
+        if self.current_token.type in (BREAK, IF, SWITCH, WHILE, DO, FOR, ID, INTEGER_TYPE, CONSTANT, SEMI_COLON):
+            nodes = self.statement_list()
         self.eat(R_BRACE)
 
         root = Compound()
@@ -409,6 +495,112 @@ class Parser(object):
         right = self.expr()
         node = Assign(left, token, right)
         return node
+
+    def selection_statement(self):
+        """
+        selection_statement : IF L_PAREN boolean_expression R_PAREN compound_statement
+                            | IF L_PAREN boolean_expression R_PAREN compound_statement ELSE compound_statement
+                            | SWITCH L_PAREN variable R_PAREN L_BRACE (CASE (INTEGER_VALUE | MINUS INTEGER_VALUE) COLON compound_statement)+ (DEFAULT COLON compound_statement) R_BRACE
+        """
+        node = NoOp()
+        if self.current_token.type == IF:
+            self.eat(IF)
+            self.eat(L_PAREN)
+            self.bool_expr = self.boolean_expression()
+            self.eat(R_PAREN)
+            self.cmpd_stat1 = self.compound_statement()
+            self.cmpd_stat2 = None
+            if self.current_token.type == ELSE:
+                self.eat(ELSE)
+                self.cmpd_stat2 = self.compound_statement()
+            node = IfStat(self.expr, self.cmpd_stat1, self.cmpd_stat2)
+            return node
+        elif self.current_token.type == SWITCH:
+            self.eat(SWITCH)
+            self.eat(L_PAREN)
+            self.var = self.variable()
+            self.eat(R_PAREN)
+            self.eat(L_BRACE)
+            self.eat(CASE)
+            self.case_stats = []
+            self.cmpd_stats = []
+            if self.current_token.type == INTEGER_VALUE:
+                num_token = self.current_token
+                self.eat(INTEGER_VALUE)
+                self.eat(COLON)
+                self.cmpd_stats.append(self.compound_statement())
+                self.case_stats.append(Number(num_token))
+            elif self.current_token.type == MINUS:
+                minus_token = self.current_token
+                self.eat(MINUS)
+                num_token = self.current_token
+                self.eat(INTEGER_VALUE)
+                self.eat(COLON)
+                self.cmpd_stats.append(self.compound_statement())
+                self.case_stats.append(UnaryOp(minus_token, Number(num_token)))
+            while self.current_token.type == CASE:
+                self.eat(CASE)
+                if self.current_token.type == INTEGER_VALUE:
+                    num_token = self.current_token
+                    self.eat(INTEGER_VALUE)
+                    self.eat(COLON)
+                    self.cmpd_stats.append(self.compound_statement())
+                    self.case_stats.append(Number(num_token))
+                elif self.current_token.type == MINUS:
+                    minus_token = self.current_token
+                    self.eat(MINUS)
+                    num_token = self.current_token
+                    self.eat(INTEGER_VALUE)
+                    self.eat(COLON)
+                    self.cmpd_stats.append(self.compound_statement())
+                    self.case_stats.append(UnaryOp(minus_token, Number(num_token)))
+            if self.current_token.type == DEFAULT:
+                default_token = self.current_token
+                self.eat(DEFAULT)
+                self.eat(COLON)
+                self.cmpd_stats.append(self.compound_statement())
+                self.case_stats.append(default_token)
+            self.eat(R_BRACE)
+            node = SwitchStat(self.var, self.case_stats, self.cmpd_stats)
+            return node
+
+    def iteration_statement(self):
+        """
+        iteration_statement : WHILE L_PAREN boolean_expression R_PAREN compound_statement
+                            | DO compound_statement WHILE L_PAREN boolean_expression R_PAREN SEMI_COLON
+                            | FOR L_PAREN init_for SEMI_COLON boolean_expression SEMI_COLON assignment_statement R_PAREN compound_statement
+        """
+        node = NoOp()
+        if self.current_token.type == WHILE:
+            self.eat(WHILE)
+            self.eat(L_PAREN)
+            self.bool_expr = self.boolean_expression()
+            self.eat(R_PAREN)
+            self.cmpd_stat = self.compound_statement()
+            node = WhileStat(self.expr, self.cmpd_stat)
+            return node
+        elif self.current_token.type == DO:
+            self.eat(DO)
+            self.cmpd_stat = self.compound_statement()
+            self.eat(WHILE)
+            self.eat(L_PAREN)
+            self.bool_expr = self.boolean_expression()
+            self.eat(R_PAREN)
+            self.eat(SEMI_COLON)
+            node = DoWhileStat(self.expr, self.cmpd_stat)
+            return node
+        elif self.current_token.type == FOR:
+            self.eat(FOR)
+            self.eat(L_PAREN)
+            self.init = self.init_for()
+            self.eat(SEMI_COLON)
+            self.middle = self.boolean_expression()
+            self.eat(SEMI_COLON)
+            self.end = self.assignment_statement()
+            self.eat(R_PAREN)
+            self.cmpd_stat = self.compound_statement()
+            node = ForStat(self.init, self.middle, self.end, self.cmpd_stat)
+            return node
 
     def variable_declaration(self):
         """
@@ -509,6 +701,67 @@ class Parser(object):
             node = self.variable()
             return node
 
+    def boolean_expression(self):
+        """
+        boolean_expression : boolean_term (relation boolean_term)*
+        """
+        node = self.boolean_term()
+
+        while self.current_token.type in (AND, OR, NOT, EQUAL, NOT_EQUAL, LESS_EQUAL, GREATER_EQUAL, LESS, GREATER):
+            token = self.current_token
+            if token.type == AND:
+                self.eat(AND)
+            elif token.type == OR:
+                self.eat(OR)
+            elif token.type == NOT:
+                self.eat(NOT)
+            elif token.type == EQUAL:
+                self.eat(EQUAL)
+            elif token.type == NOT_EQUAL:
+                self.eat(NOT_EQUAL)
+            elif token.type == LESS_EQUAL:
+                self.eat(LESS_EQUAL)
+            elif token.type == GREATER_EQUAL:
+                self.eat(GREATER_EQUAL)
+            elif token.type == LESS:
+                self.eat(LESS)
+            elif token.type == GREATER:
+                self.eat(GREATER)
+
+            node = BoolOp(left=node, op=token, right=self.boolean_term())
+
+        return node
+
+    def boolean_term(self):
+        """
+        boolean_term : NOT boolean_term
+                     | TRUE
+                     | FALSE
+                     | L_PAREN boolean_expression R_PAREN
+        """
+        token = self.current_token
+        if token.type == NOT:
+            self.eat(NOT)
+            node = UnaryOp(token, self.boolean_term)
+            return node
+        elif token.type == TRUE:
+            self.eat(TRUE)
+            return Boolean(token)
+        elif token.type == FALSE:
+            self.eat(FALSE)
+            return Boolean(token)
+        elif token.type == L_PAREN:
+            self.eat(L_PAREN)
+            node = self.boolean_expression()
+            self.eat(R_PAREN)
+            return node
+        elif token.type == INTEGER_VALUE:
+            self.eat(INTEGER_VALUE)
+            return Number(token)
+        else:
+            node = self.variable()
+            return node
+
     def parse(self):
         node = self.program()
         if self.current_token.type != EOF:
@@ -544,65 +797,87 @@ class Interpreter(NodeVisitor):
 
 
     def visit_VarDecl(self, node):
+        global label_id
         var_name = node.left.value
         if node.right:
             value = self.visit(node.right)
             self.GLOBAL_SCOPE[var_name] = value
-            out_file.write("(=, " + str(value) + ", , " + str(var_name) + ")\n")
+            out_file.write(str(label_id) + ": (=, " + str(value) + ", , " + str(var_name) + ")\n")
+            label_id = label_id + 1
 
     def visit_Type(self, node):
         # Do nothing
         pass
 
     def visit_BinOp(self, node):
-        global register_id
+        global register_id, label_id
         if node.op.type == PLUS:
             # value = self.visit(node.left) + self.visit(node.right)
-            out_file.write('(+, ' + str(self.visit(node.left)) + ', ' + str(self.visit(node.right)) + ', R' + str(register_id) + ')\n')
+            left_val = str(self.visit(node.left))
+            right_val = str(self.visit(node.right))
+            destination_register = 'R' + str(register_id)
+            out_file.write(str(label_id) + ': (+, ' + left_val + ', ' + right_val + ', ' + destination_register + ')\n')
             register_id = register_id + 1
-            return 'R' + str(register_id - 1)
+            label_id = label_id + 1
+            return destination_register
         elif node.op.type == MINUS:
             # value = self.visit(node.left) - self.visit(node.right)
-            out_file.write('(-, ' + str(self.visit(node.left)) + ', ' + str(self.visit(node.right)) + ', R' + str(register_id) + ')\n')
+            left_val = str(self.visit(node.left))
+            right_val = str(self.visit(node.right))
+            destination_register = 'R' + str(register_id)
+            out_file.write(str(label_id) + ': (-, ' + left_val + ', ' + right_val + ', ' + destination_register + ')\n')
             register_id = register_id + 1
-            return 'R' + str(register_id - 1)
+            label_id = label_id + 1
+            return destination_register
         elif node.op.type == MULTIPLY:
             # value = self.visit(node.left) * self.visit(node.right)
-            out_file.write('(*, ' + str(self.visit(node.left)) + ', ' + str(self.visit(node.right)) + ', R' + str(register_id) + ')\n')
+            left_val = str(self.visit(node.left))
+            right_val = str(self.visit(node.right))
+            destination_register = 'R' + str(register_id)
+            out_file.write(str(label_id) + ': (*, ' + left_val + ', ' + right_val + ', ' + destination_register + ')\n')
             register_id = register_id + 1
-            return 'R' + str(register_id - 1)
+            label_id = label_id + 1
+            return destination_register
         elif node.op.type == DIVIDE:
             # value = self.visit(node.left) / self.visit(node.right)
-            out_file.write('(/, ' + str(self.visit(node.left)) + ', ' + str(self.visit(node.right)) + ', R' + str(register_id) + ')\n')
+            left_val = str(self.visit(node.left))
+            right_val = str(self.visit(node.right))
+            destination_register = 'R' + str(register_id)
+            out_file.write(str(label_id) + ': (/, ' + left_val + ', ' + right_val + ', ' + destination_register + ')\n')
             register_id = register_id + 1
-            return 'R' + str(register_id - 1)
+            label_id = label_id + 1
+            return destination_register
 
     def visit_Number(self, node):
         return str(node.value)
 
     def visit_UnaryOp(self, node):
-        global register_id
+        global register_id, label_id
         op = node.op.type
         if op == PLUS:
             return '' + str(self.visit(node.expr))
         elif op == MINUS:
             # return '-' + str(self.visit(node.expr))
-            out_file.write('(uminus, ' + str(self.visit(node.expr)) + ', , R' + str(register_id) + ')\n')
+            value = str(self.visit(node.expr))
+            destination_register = 'R' + str(register_id)
+            out_file.write(str(label_id) + ': (uminus, ' + value + ', , ' + destination_register + ')\n')
             register_id = register_id + 1
-            return 'R' + str(register_id - 1)
+            label_id = label_id + 1
+            return destination_register
 
     def visit_Compound(self, node):
         for child in node.children:
             self.visit(child)
 
     def visit_Assign(self, node):
-        global register_id
+        global register_id, label_id
         var_name = node.left.value
         # self.GLOBAL_SCOPE[var_name] = self.visit(node.right)
         value = self.visit(node.right)
 
         # out_file.write('(=, ' + str(var_name) + ', ' + str(self.visit(node.right)) + ', R' + str(register_id) + ')\n')
-        out_file.write("(=, " + str(value) + ", , " + str(var_name) + ")\n")
+        out_file.write(str(label_id) + ": (=, " + str(value) + ", , " + str(var_name) + ")\n")
+        label_id = label_id + 1
         # register_id = register_id + 1
         # return 'R' + str(register_id - 1)
 
@@ -617,17 +892,42 @@ class Interpreter(NodeVisitor):
     def visit_NoOp(self, node):
         return ""
 
+    def visit_Boolean(self, node):
+        return str(node.value)
+
+    def visit_SwitchStat(self, node):
+        pass
+
+    def visit_IfStat(self, node):
+        pass
+
+    def visit_WhileStat(self, node):
+        pass
+
+    def visit_DoWhileStat(self, node):
+        pass
+
+    def visit_BoolOp(self, node):
+        pass
+
+    def visit_ForStat(self, node):
+        pass
+
+    def visit_BreakStat(self, node):
+        pass
+
     def interpret(self):
         tree = self.parser.parse()
         if tree is None:
             return ''
-        return self.visit(tree)
+        # return self.visit(tree)
 
 
 def main():
+    global newLine_pos
     import sys
     text = open(sys.argv[1], 'r').read()
-
+    newLine_pos = locations_of_substring(text, '\n')
     lexer = Lexer(text)
     parser = Parser(lexer)
     interpreter = Interpreter(parser)
